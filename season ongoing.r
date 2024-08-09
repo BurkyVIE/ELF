4457aa64ad2c23f0f57392ec108983a6d89f732a
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2024-08-09 07:48:22 +0200

    Add files via upload

--- season ongoing.r
+++ season ongoing.r
@@ -18,9 +18,10 @@ dat <- filter(teaminfo_elf, Franchise == selection) |>
 ggplot(dat) +
   aes(x = Week, y = Pct, color = Franchise) +
   geom_line(linewidth = 1.5, alpha = 2/3) +
-  geom_point(size = 2.5, alpha = 3/4) +
+  geom_point(mapping = aes(shape = bye), size = 3, alpha = 3/4, show.legend = FALSE) +
   scale_x_continuous(breaks = seq(2, 20, by = 4), minor_breaks = seq(0, 20, by = 2)) +
   scale_y_continuous(lim = c(0, 1)) +
+  scale_shape_manual(values = c(19, NA)) +
   scale_color_viridis_d(option = "H") +
   # scale_color_brewer(palette = "YlOrBr") +
   facet_wrap(~Season+ConfDiv, ncol = 2) +
