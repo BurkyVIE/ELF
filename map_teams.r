3d399a6764872d3daced1e9756a0ab19569b3881
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2024-12-14 23:24:00 +0100

    Add files via upload

--- map_teams.r
+++ map_teams.r
@@ -8,9 +8,15 @@ library(ggrepel)
 source("teams.r")
 
 # DATA ----
-seas <- 2024
+seas <- 2025
 
-data <- teamloc_elf %>% filter(Season == seas)
+# data <- teamloc_elf %>% filter(Season == seas)
+data <- teamloc_elf |>
+  filter(Season == seas) |>
+  group_by(Franchise) |>
+  summarise(Lat = as.vector(t(Gs) %*% Lat), Long = as.vector(t(Gs) %*% Long),
+            Conference = first(Conference),
+            Division = first())
 
 mapdata <- ne_countries(scale = 50, returnclass = "sf")
 
@@ -21,12 +27,12 @@ with(data %>% filter(!is.na(Long)), SpatialPoints(cbind(Long, Lat))) %>%
 p <- ggplot() +
   geom_sf(data = mapdata, fill = "grey90", col = "grey30", size = 1.25) +
   # geom_point(data, mapping = aes(x = Long, y = Lat), color = "red3", size = 4) +
-  geom_label_repel(data, mapping = aes(x = Long, y = Lat, label = Franchise, fill = Conference), colour = "white",
+  geom_label_repel(data, mapping = aes(x = Long, y = Lat, label = Franchise, fill = Division), colour = "black",
                    box.padding = unit(.6, "lines"), point.padding = unit(.4, "lines"), force_pull = .5,
                    label.r = unit(0.5, "lines"), label.size = NA, fontface = "bold", show.legend = TRUE) +
   ggimage::geom_image(data, mapping = aes(x = Long, y = Lat), image = "fb_32.png", size = .02) +
   # scale_fill_manual(name = "Conference", values = c(Western = "#1f3f77", Central = "#008000", Eastern = "#cd2028"), na.value = "#a39819") +
-  scale_fill_manual(name = "Conference", values = colors_elf$Conference |> filter(Season == seas) |> select(Name, Color) |> deframe(), na.value = "#a39819") +
+  scale_fill_manual(name = "Division", values = colors_elf$Division |> filter(Season == seas) |> select(Name, Color) |> deframe(), na.value = "#a39819") +
   scale_x_continuous(name = "Longitude", expand = expansion(mult = .05), labels = (\(x) paste0(x, "°", ifelse(x < 0, "W", "E")))) + # solves problems with incorrect interpretation of 'degrees'
   scale_y_continuous(name = "Latitude", expand = expansion(mult = .05), labels = (\(y) paste0(y, "°", ifelse(y < 0, "S", "N")))) +
   coord_sf(bbo[1,], bbo[2,], expand = TRUE) +
