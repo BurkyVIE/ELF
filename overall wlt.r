fd25f514a8f2d6d72d19caec567affbcd820a40e
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2025-05-19 07:18:18 +0200

    Update overall wlt.r

--- overall wlt.r
+++ overall wlt.r
@@ -9,10 +9,11 @@ results |>
   group_by(Franchise) |> 
   summarise(Results = list(Result), .groups = "drop",
             Gs = n(),
-            Last_active = last(Season),
+            Active = last(Season),
             PFc = sum(PF),
             PAc = sum(PA)) |> 
   mutate(map_df(Results, ~unlist(.) %>% table(.)),
+         Active = sign(Active - max(Active)) == 0,
          across(W:T, as.integer),
          Pct = (W + 1/2 * T) / (W + L + T),
          PRat = PFc / (PFc + PAc),
@@ -26,11 +27,11 @@ print(res)
 
 # plot ----
   ggplot(data = res) +
-  aes(x = WRat, y = Pyt, color = Last_active) +
+  aes(x = WRat, y = Pyt, color = Active) +
   geom_abline(slope = , intercept = 0, linetype = "dashed") +
-  geom_point(size = 2) +
-  ggrepel::geom_label_repel(mapping = aes(label = Franchise, fill = Delta * 10), show.legend = FALSE) +
-  scale_color_distiller(palette = "Greys", direction = 1) +
+  geom_point(size = 2, show.legend = FALSE) +
+  ggrepel::geom_label_repel(mapping = aes(label = Franchise, fill = Delta * 10)) +
+  scale_color_manual(values = c("grey40", "black"), guide = "none") +
   scale_x_continuous(name = "True Win Ratio") +
   scale_y_continuous(name = "Expectetd Win Ratio (Pythagorean)") +
   scale_fill_distiller(name = "Overwinning\nper 10 Games", palette = "RdYlGn", direction = 1, values = scales::rescale(c(range(res$Delta), 0)[c(1, 3, 2)])) +
