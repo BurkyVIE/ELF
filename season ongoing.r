fae44da19f5557ab0dde9053307826c2f8ee895b
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2025-08-20 08:35:48 +0200

    Update season ongoing.r

--- season ongoing.r
+++ season ongoing.r
@@ -3,6 +3,7 @@ library(tidyverse)
 
 # INPUT ----
 selection <- "Vikings"
+colo <- "purple"
 
 # DATA ----
 dat <- filter(teaminfo_elf, Franchise == selection) |> 
@@ -17,14 +18,14 @@ dat <- filter(teaminfo_elf, Franchise == selection) |>
 
 # VISUALISE ----
 ggplot(dat) +
-  aes(x = Week, y = Pct, group = Franchise, color = Franchise == "Vikings") +
+  aes(x = Week, y = Pct, group = Franchise, color = Franchise == selection) +
   # geom_line(linewidth = 1.5, alpha = .85) +
   geomtextpath::geom_textline(aes(label = Abb3), size = 4, alpha = .85, linewidth = 1.5, hjust = .99, show.legend = FALSE) +
   geom_point(mapping = aes(shape = bye), size = 3, alpha = .85, show.legend = FALSE) +
   scale_x_continuous(breaks = seq(2, 20, by = 4), minor_breaks = seq(0, 20, by = 2)) +
   scale_y_continuous(lim = c(0, 1)) +
   scale_shape_manual(values = c(19, NA)) +
-  scale_color_manual(values = c("grey50", "purple")) +
+  scale_color_manual(values = c("grey50", colo)) +
   facet_wrap(~Season+ConfDiv, ncol = 2) +
   labs(title = paste("ELF RS Ongoing for the", selection),
        subtitle = "Opponents according to Conference/Division") +theme_bw(base_size = 13) +
@@ -36,4 +37,5 @@ windows(16, 9)
 plot(p)
 
 # CLEAN UP ----
+
 rm(selection, dat, p)
